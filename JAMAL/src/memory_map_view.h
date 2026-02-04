36a336050856233498454680a055976ebe404522
Mat Dalgleish <mat.dalgleish@staffs.ac.uk>
2026-02-04 20:24:24 +0000

    Delete JAMAL directory

--- JAMAL/src/memory_map_view.h
+++
@@ -1,8 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-@interface MemoryMapView : NSView
-- (void)tickWithLevel:(float)level;
-- (void)setDayMode:(BOOL)isDay;
-- (void)triggerPortal;
-- (void)setBeatPulse:(float)pulse;
-@end
--- JAMAL/src/meter_view.h
+++
@@ -1,7 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-@interface MeterView : NSView
-@property (nonatomic, assign) float level; // RMS 0..1
-@property (nonatomic, assign) float peak;  // Peak 0..1+
-@property (nonatomic, assign) BOOL clip;
-@end
